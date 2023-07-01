14fe66922753ca29bf899656a3b8f60dcaa9e65b
MushfikYeasirPk <pkm2243@mail.com>
2023-07-01 13:51:52 +0600

    cardDeal added

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => (
         <Stats />
         <Business />
         <Billing />
-        {/* <CardDeal /> */}
+        <CardDeal />
         {/* <Testimonials /> */}
         {/* <Clients /> */}
         {/* <CTA /> */}
